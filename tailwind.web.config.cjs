a419c5194fb8ab02f5e60d072a9c21140cd04a5c
Stepheno <fireflyengineer@gmail.com>
2025-10-16 21:56:24 +0800

    feat: integrate tailwindcss

---
+++ tailwind.web.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./App.uvue",
+    "./pages/**/*.uvue",
+    "./components/**/*.uvue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
